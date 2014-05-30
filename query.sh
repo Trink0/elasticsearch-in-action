03fb017addad35ac20e32fa0f076dfa02d2c4cb8
Marco Trincardi <m@g.com>
2014-05-30 13:14:15 +0200

    adding query match

--- query.sh
+++ query.sh
@@ -1,10 +1,45 @@
 #!/bin/bash
-curl '192.168.10.10:9200/get-together/_search' -d '{
+ES_HOST="192.168.10.10"
+
+echo "###############"
+echo "Query Match host"
+echo "###############"
+curl $ES_HOST':9200/get-together/_search' -d '{
 	"query": {
 		"match": {
-			"organizer": "Lee"
+			"host": "Lee"
+		}
+	}
+}'
+echo
+
+echo "###############"
+echo "Query Match with cursor starting from 3"
+echo "###############"
+curl $ES_HOST':9200/get-together/_search' -d '{
+	"query": {
+		"match": {
+			"host": "Lee"
 		}
 	},
-	"from": 10,
+
+	"from": 3,
 	"size": 10
 }'
+echo
+
+echo "###############"
+echo "Query Match with cursor starting from 3, returning only Name and description fields"
+echo "###############"
+curl $ES_HOST':9200/get-together/_search' -d '{
+	"query": {
+		"match": {
+			"host": "Lee"
+		}
+	},
+	"fields": ["name", "description"],
+
+	"from": 3,
+	"size": 10
+}'
+echo
